ba2d541ecf37ed0326a3fcad2ef42f36d1fe0db3
torkvenick <oomsnikita@gmail.com>
2020-08-15 01:10:36 +0200

    finished Filter p.2

--- js/main.js
+++ js/main.js
@@ -35,7 +35,7 @@ $(function(){
  
     $('.filter-style').styler();
 
-    $('.filter__item-drop').on('click', function(){
+    $('.filter__item-drop, .filter__extra').on('click', function(){
         $(this).toggleClass('filter__item-drop--active');
         $(this).next().slideToggle(200);
     });
@@ -44,6 +44,18 @@ $(function(){
         type: "double",
         min: 100000,
         max: 500000,
+    });
+
+    $('.catalog__filter-btngrid').on('click', function(){
+        $(this).addClass('catalog__filter-button--active');
+        $('.catalog__filter-btnline').removeClass('catalog__filter-button--active');
+        $('.product-item__wrapper').removeClass('product-item__wrapper--list');
+    });
+
+    $('.catalog__filter-btnline').on('click', function(){
+        $(this).addClass('catalog__filter-button--active');
+        $('.catalog__filter-btngrid').removeClass('catalog__filter-button--active');
+        $('.product-item__wrapper').addClass('product-item__wrapper--list');
     });
 
 });
